746db6bd12b87f822878b0f39406412aa9b9d17a
he.wu <he.wu@woven-planet.global>
2022-04-25 18:18:50 +0900

    add auto-generated dart code

---
+++ protobuf/gen/dart/out/protobuf/addressbook.pbenum.dart
@@ -0,0 +1,68 @@
+///
+//  Generated code. Do not modify.
+//  source: protobuf/addressbook.proto
+//
+// @dart = 2.12
+// ignore_for_file: annotate_overrides,camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type,unnecessary_this,prefer_final_fields
+
+// ignore_for_file: UNDEFINED_SHOWN_NAME
+import 'dart:core' as $core;
+import 'package:protobuf/protobuf.dart' as $pb;
+
+class SearchRequest_Corpus extends $pb.ProtobufEnum {
+  static const SearchRequest_Corpus UNIVERSAL = SearchRequest_Corpus._(0, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'UNIVERSAL');
+  static const SearchRequest_Corpus WEB = SearchRequest_Corpus._(1, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'WEB');
+  static const SearchRequest_Corpus IMAGES = SearchRequest_Corpus._(2, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'IMAGES');
+  static const SearchRequest_Corpus LOCAL = SearchRequest_Corpus._(3, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'LOCAL');
+  static const SearchRequest_Corpus NEWS = SearchRequest_Corpus._(4, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'NEWS');
+  static const SearchRequest_Corpus PRODUCTS = SearchRequest_Corpus._(5, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'PRODUCTS');
+  static const SearchRequest_Corpus VIDEO = SearchRequest_Corpus._(6, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'VIDEO');
+
+  static const $core.List<SearchRequest_Corpus> values = <SearchRequest_Corpus> [
+    UNIVERSAL,
+    WEB,
+    IMAGES,
+    LOCAL,
+    NEWS,
+    PRODUCTS,
+    VIDEO,
+  ];
+
+  static final $core.Map<$core.int, SearchRequest_Corpus> _byValue = $pb.ProtobufEnum.initByValue(values);
+  static SearchRequest_Corpus? valueOf($core.int value) => _byValue[value];
+
+  const SearchRequest_Corpus._($core.int v, $core.String n) : super(v, n);
+}
+
+class MyMessage1_EnumAllowingAlias extends $pb.ProtobufEnum {
+  static const MyMessage1_EnumAllowingAlias UNKNOWN = MyMessage1_EnumAllowingAlias._(0, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'UNKNOWN');
+  static const MyMessage1_EnumAllowingAlias STARTED = MyMessage1_EnumAllowingAlias._(1, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'STARTED');
+
+  static const MyMessage1_EnumAllowingAlias RUNNING = STARTED;
+
+  static const $core.List<MyMessage1_EnumAllowingAlias> values = <MyMessage1_EnumAllowingAlias> [
+    UNKNOWN,
+    STARTED,
+  ];
+
+  static final $core.Map<$core.int, MyMessage1_EnumAllowingAlias> _byValue = $pb.ProtobufEnum.initByValue(values);
+  static MyMessage1_EnumAllowingAlias? valueOf($core.int value) => _byValue[value];
+
+  const MyMessage1_EnumAllowingAlias._($core.int v, $core.String n) : super(v, n);
+}
+
+class MyMessage2_EnumNotAllowingAlias extends $pb.ProtobufEnum {
+  static const MyMessage2_EnumNotAllowingAlias UNKNOWN = MyMessage2_EnumNotAllowingAlias._(0, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'UNKNOWN');
+  static const MyMessage2_EnumNotAllowingAlias STARTED = MyMessage2_EnumNotAllowingAlias._(1, const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'STARTED');
+
+  static const $core.List<MyMessage2_EnumNotAllowingAlias> values = <MyMessage2_EnumNotAllowingAlias> [
+    UNKNOWN,
+    STARTED,
+  ];
+
+  static final $core.Map<$core.int, MyMessage2_EnumNotAllowingAlias> _byValue = $pb.ProtobufEnum.initByValue(values);
+  static MyMessage2_EnumNotAllowingAlias? valueOf($core.int value) => _byValue[value];
+
+  const MyMessage2_EnumNotAllowingAlias._($core.int v, $core.String n) : super(v, n);
+}
+
---
+++ protobuf/gen/dart/out/protobuf/addressbook.pbjson.dart
@@ -0,0 +1,107 @@
+///
+//  Generated code. Do not modify.
+//  source: protobuf/addressbook.proto
+//
+// @dart = 2.12
+// ignore_for_file: annotate_overrides,camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type,unnecessary_this,prefer_final_fields,deprecated_member_use_from_same_package
+
+import 'dart:core' as $core;
+import 'dart:convert' as $convert;
+import 'dart:typed_data' as $typed_data;
+@$core.Deprecated('Use searchRequestDescriptor instead')
+const SearchRequest$json = const {
+  '1': 'SearchRequest',
+  '2': const [
+    const {'1': 'query', '3': 1, '4': 1, '5': 9, '10': 'query'},
+    const {'1': 'page_number', '3': 2, '4': 1, '5': 5, '10': 'pageNumber'},
+    const {'1': 'result_per_page', '3': 3, '4': 1, '5': 5, '10': 'resultPerPage'},
+    const {'1': 'corpus', '3': 4, '4': 1, '5': 14, '6': '.SearchRequest.Corpus', '10': 'corpus'},
+  ],
+  '4': const [SearchRequest_Corpus$json],
+};
+
+@$core.Deprecated('Use searchRequestDescriptor instead')
+const SearchRequest_Corpus$json = const {
+  '1': 'Corpus',
+  '2': const [
+    const {'1': 'UNIVERSAL', '2': 0},
+    const {'1': 'WEB', '2': 1},
+    const {'1': 'IMAGES', '2': 2},
+    const {'1': 'LOCAL', '2': 3},
+    const {'1': 'NEWS', '2': 4},
+    const {'1': 'PRODUCTS', '2': 5},
+    const {'1': 'VIDEO', '2': 6},
+  ],
+};
+
+/// Descriptor for `SearchRequest`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List searchRequestDescriptor = $convert.base64Decode('Cg1TZWFyY2hSZXF1ZXN0EhQKBXF1ZXJ5GAEgASgJUgVxdWVyeRIfCgtwYWdlX251bWJlchgCIAEoBVIKcGFnZU51bWJlchImCg9yZXN1bHRfcGVyX3BhZ2UYAyABKAVSDXJlc3VsdFBlclBhZ2USLQoGY29ycHVzGAQgASgOMhUuU2VhcmNoUmVxdWVzdC5Db3JwdXNSBmNvcnB1cyJaCgZDb3JwdXMSDQoJVU5JVkVSU0FMEAASBwoDV0VCEAESCgoGSU1BR0VTEAISCQoFTE9DQUwQAxIICgRORVdTEAQSDAoIUFJPRFVDVFMQBRIJCgVWSURFTxAG');
+@$core.Deprecated('Use myMessage1Descriptor instead')
+const MyMessage1$json = const {
+  '1': 'MyMessage1',
+  '4': const [MyMessage1_EnumAllowingAlias$json],
+};
+
+@$core.Deprecated('Use myMessage1Descriptor instead')
+const MyMessage1_EnumAllowingAlias$json = const {
+  '1': 'EnumAllowingAlias',
+  '2': const [
+    const {'1': 'UNKNOWN', '2': 0},
+    const {'1': 'STARTED', '2': 1},
+    const {'1': 'RUNNING', '2': 1},
+  ],
+  '3': const {'2': true},
+};
+
+/// Descriptor for `MyMessage1`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List myMessage1Descriptor = $convert.base64Decode('CgpNeU1lc3NhZ2UxIj4KEUVudW1BbGxvd2luZ0FsaWFzEgsKB1VOS05PV04QABILCgdTVEFSVEVEEAESCwoHUlVOTklORxABGgIQAQ==');
+@$core.Deprecated('Use myMessage2Descriptor instead')
+const MyMessage2$json = const {
+  '1': 'MyMessage2',
+  '4': const [MyMessage2_EnumNotAllowingAlias$json],
+};
+
+@$core.Deprecated('Use myMessage2Descriptor instead')
+const MyMessage2_EnumNotAllowingAlias$json = const {
+  '1': 'EnumNotAllowingAlias',
+  '2': const [
+    const {'1': 'UNKNOWN', '2': 0},
+    const {'1': 'STARTED', '2': 1},
+  ],
+};
+
+/// Descriptor for `MyMessage2`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List myMessage2Descriptor = $convert.base64Decode('CgpNeU1lc3NhZ2UyIjAKFEVudW1Ob3RBbGxvd2luZ0FsaWFzEgsKB1VOS05PV04QABILCgdTVEFSVEVEEAE=');
+@$core.Deprecated('Use searchResponseDescriptor instead')
+const SearchResponse$json = const {
+  '1': 'SearchResponse',
+  '2': const [
+    const {'1': 'results', '3': 1, '4': 3, '5': 11, '6': '.Result', '10': 'results'},
+  ],
+};
+
+/// Descriptor for `SearchResponse`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List searchResponseDescriptor = $convert.base64Decode('Cg5TZWFyY2hSZXNwb25zZRIhCgdyZXN1bHRzGAEgAygLMgcuUmVzdWx0UgdyZXN1bHRz');
+@$core.Deprecated('Use resultDescriptor instead')
+const Result$json = const {
+  '1': 'Result',
+  '2': const [
+    const {'1': 'url', '3': 1, '4': 1, '5': 9, '10': 'url'},
+    const {'1': 'title', '3': 2, '4': 1, '5': 9, '10': 'title'},
+    const {'1': 'snippets', '3': 3, '4': 3, '5': 9, '10': 'snippets'},
+  ],
+};
+
+/// Descriptor for `Result`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List resultDescriptor = $convert.base64Decode('CgZSZXN1bHQSEAoDdXJsGAEgASgJUgN1cmwSFAoFdGl0bGUYAiABKAlSBXRpdGxlEhoKCHNuaXBwZXRzGAMgAygJUghzbmlwcGV0cw==');
+@$core.Deprecated('Use errorStatusDescriptor instead')
+const ErrorStatus$json = const {
+  '1': 'ErrorStatus',
+  '2': const [
+    const {'1': 'message', '3': 1, '4': 1, '5': 9, '10': 'message'},
+    const {'1': 'details', '3': 2, '4': 3, '5': 11, '6': '.google.protobuf.Any', '10': 'details'},
+  ],
+};
+
+/// Descriptor for `ErrorStatus`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List errorStatusDescriptor = $convert.base64Decode('CgtFcnJvclN0YXR1cxIYCgdtZXNzYWdlGAEgASgJUgdtZXNzYWdlEi4KB2RldGFpbHMYAiADKAsyFC5nb29nbGUucHJvdG9idWYuQW55UgdkZXRhaWxz');
